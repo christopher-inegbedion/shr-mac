620222657466f64f2306a2d3d78e6229b0768368
Christopher Inegbedion <64270272+christopher-inegbedion@users.noreply.github.com>
2023-02-13 20:29:42 +0000

    Initial Commit

---
+++ Shr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Shr
+//
+//  Created by Christopher Inegbedion on 13/02/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
